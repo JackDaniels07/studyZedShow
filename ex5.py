3a52ae74a5348ad565437727b2627c573712ae23
Denis <Denis-life@yandex.ru>
2020-03-24 00:29:53 +0300

    ех5

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = "Зед Шоу"
+my_age = 35
+my_height = 188
+my_weight = 80
+my_eyes = 'голубые'
+my_teeth = "Белые"
+my_hair = 'Каштановые'
+
+print(f"Давайте поговорим о человеке по имени {my_name}.")
+print(f"Его рост составляет {my_height} см.")
+print(f"Он весит {my_weight} кг.")
+print("На самом деле это не так и много.")
+print(f'у него {my_eyes} глаза и {my_hair} волосы.')
+print(f'его зубы обычно {my_teeth}, хотя он и любит пить кофе.')
+#эта строка кода довольна сложная, попробую не ошибиться
+total = my_age + my_height + my_weight
+print(f'Если я сложу {my_age}, {my_height} и {my_weight}, то получу {total}.')
+
