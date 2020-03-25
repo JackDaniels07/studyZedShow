b0df1517ec143af3b8bf7668672728efa22d77f7
Denis <Denis-life@yandex.ru>
2020-03-25 22:59:48 +0300

    double

---
+++ ex10.py
@@ -0,0 +1,17 @@
+print("Брюки Levi's с обхватом талии 30")
+
+d_artagnan = "\t Меня зовут Д'Артаньян"
+athos = "Эта строка \nразделена на две."
+portos = "Я \\ - \\ мушкетер!"
+
+aramis = """
+Подсказки:
+\t* Граф гаскони
+\t* Связан с миледи
+\t* Барон \n\t* генерал Ордена
+"""
+
+print(d_artagnan)
+print(athos)
+print(portos)
+print(aramis)
