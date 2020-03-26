7a327e0ab8caff24d370bf32077d99083ee5f40c
Denis <Denis-life@yandex.ru>
2020-03-26 23:42:39 +0300

    ex13.py

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+#Прочитайте раздел результат чтобы понять, как запустить этот скрипт
+
+script, first, second, third = argv
+print("Этот сценарий называется:", script)
+print("Моя первая переменная:", first)
+print("Моя вторая переменная:", second)
+print("Моя третья переменная:", third)
